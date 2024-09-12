79431a2617675b0d349d49c76f53c81e492cd586
HadesSoleil0462 <90576992+HadesSoleil0462@users.noreply.github.com>
2024-09-12 10:39:43 +0200

    [ADD] dependencies in the pom.xml [ADD] DataFetcher class [ADD] CalendarGenerator class [ADD] CalendarServer class

---
+++ src/main/java/fr/tfr/CalendarServer.java
@@ -0,0 +1,8 @@
+package fr.tfr;
+
+public class CalendarServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- src/main/java/fr/tfr/Main.java
+++ src/main/java/fr/tfr/Main.java
@@ -1,7 +1,10 @@
 package fr.tfr;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+
 }
+
